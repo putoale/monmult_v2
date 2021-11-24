b32ea01f49deff79ea6d9598fab51fe244560623
Alessandro Putortì <putoale@gmail.com>
2021-11-24 13:23:38 +0100

    FSM_add completed (still to be commented)

--- Sources/RTL/FSM_add.vhd
+++ Sources/RTL/FSM_add.vhd
@@ -50,13 +50,22 @@ architecture Behavioral of FSM_add is
   end component;
 
 
-type state_type is (IDLE,SUM_1,SUM_2,SUM_3);
+type state_type is (IDLE,SUM_1,SUM_1B,SUM_2,SUM_3);
 signal state : state_type := IDLE;
 
-signal a_sig : std_logic_vector (N_BITS_PER_WORD-1 downto 0) := (Others =>'0');
-signal b_sig : std_logic_vector (N_BITS_PER_WORD-1 downto 0) := (Others =>'0');
+signal a_sig : std_logic_vector (N_BITS_PER_WORD-1 downto 0) := (Others =>'0'); -- used to load carry from outside
+signal b_sig : std_logic_vector (N_BITS_PER_WORD-1 downto 0) := (Others =>'0'); -- used to load internal values
 
+signal start_reg : std_logic := '0';
+signal c_out_reg : std_logic_vector(N_BITS_PER_WORD-1 downto 0):= (Others =>'0');
 
+signal t_out_sig : std_logic_vector(t_out'range) := (Others =>'0');
+signal c_out_sig : std_logic_vector(c_out'range) := (Others =>'0');
+
+signal delay_counter : unsigned (9 downto 0) := (Others =>'0');
+signal i_counter : unsigned (9 downto 0) := (Others =>'0');
+
+constant DELAY_SUM_3_1 : UNSIGNED (9 downto 0) := to_unsigned((N_WORDS - 4),10);
 
 begin
 
@@ -69,18 +78,97 @@ Port map(
           a => a_sig,
           b => b_sig,
 
-          s => t_out,
-          c => c_out
+          s => t_out_sig,
+          c => c_out_sig
 );
 
+
+t_out <= t_out_sig;
+c_out <= c_out_sig;
 
 FSM: process(clk,reset)
 begin
 
   if reset = '1' then
     state <= IDLE;
+    start_reg <= '0';
+    delay_counter <=  (Others =>'0');
+    i_counter <=  (Others => '0');
+
 
   elsif rising_edge(clk) then
+
+
+    case state is
+
+      when IDLE  =>
+
+        if start = '1' then
+          start_reg <= '1';
+        end if;
+
+        if start_reg = '1' then
+
+          if delay_counter < N_WORDS-1 then
+            delay_counter <= delay_counter + 1;
+          else
+            delay_counter <= (Others => '0');
+
+            a_sig <= c_in_ab; --carry in from MAC_AB
+            b_sig <= (Others =>'0'); -- Previous i-cycle result (0 since it's first cycle)
+
+            state <= SUM_1;
+
+          end if;
+
+        end if;
+
+
+
+
+      when SUM_1 =>
+        -- when here you have the result of SUM_1
+
+        b_sig <= t_out_sig;
+        c_out_reg <= c_out_sig;
+
+        state <= SUM_1B;
+
+
+
+      when SUM_1B =>
+        a_sig <= c_in_mn;
+
+        state <= SUM_2;
+
+
+      when SUM_2 =>
+        -- here you have the result of SUM_2
+        a_sig <= c_out_reg;
+        b_sig <= c_out_sig;
+
+        state <= SUM_3;
+
+      when SUM_3 =>
+        -- here you have the result of SUM_3
+        if delay_counter < DELAY_SUM_3_1 then
+          delay_counter <= delay_counter + 1;
+        else
+          delay_counter <= (Others =>'0');
+          b_sig <= t_out_sig;
+          a_sig <= c_in_ab;
+          state <= SUM_1;
+        end if;
+
+        if i_counter < N_WORDS-1 then
+          i_counter <= i_counter + 1;
+        else
+          i_counter <= (Others => '0');
+          start_reg <= '0';
+          state     <= IDLE;
+        end if;
+
+    end case;
 
   end if;
 
