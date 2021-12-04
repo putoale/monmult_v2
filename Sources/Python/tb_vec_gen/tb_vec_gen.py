3c5c1f44da1d92f78d1c3446581fa735045c3fa0
Alessandro Putortì <putoale@gmail.com>
2021-12-05 00:00:10 +0100

    edited python script (working, but WIP)

--- Sources/Python/tb_vec_gen/tb_vec_gen.py
+++ Sources/Python/tb_vec_gen/tb_vec_gen.py
@@ -1,18 +1,42 @@
-def n_to_str (num, n_bits_per_word, n_words):
+import gmpy2 as g
+import math  as m
+
+def n_to_str (num, n_bits_per_word, n_words,base = 2):
     """ This function takes as input a number and returns a string representing
     the number divided into n_words of n_bits_per word each"""
 
     #create a string and format it in order to have right alignment and zero padding
-    bin_num_str = bin(num)[2:].zfill(n_bits_per_word*n_words);
-    #str_list = list(bin_num_str)
-    counter = 0
+    n_symbols_per_word = (int) (n_bits_per_word / m.log(base,2))
+    bin_num_str = g.digits(num,base).zfill(n_words * n_symbols_per_word)
+
     final_str = ''
 
     for i in range(0,(n_words)):
-        final_str = final_str + bin_num_str[(i*n_bits_per_word):( ( (i+1)*n_bits_per_word )) ] + ' '
+        final_str = final_str + bin_num_str[(i*n_symbols_per_word):( ( (i+1)*n_symbols_per_word )) ] + ' '
 
-    print(final_str[:-1])
     return final_str[:-1]
+
+def monmult_str (a,b,n,r,base=16):
+    """This function computes the modular multiplication and returns it as a string"""
+
+    rr = g.invert(r,n)
+    mmult = g.f_mod(g.mul(g.mul(a,b),rr),n)
+    mult_str = g.digits(mmult,base)
+    return mult_str
+
+
+def send_tv_str (a,b,n,r,n_bits_per_word,n_words,base=16):
+
+    n_symbols_per_word = (int) (n_bits_per_word / m.log(base,2))
+
+    nn = g.digits(g.invert(-n,r),base).zfill(n_symbols_per_word*n_words)
+
+    a_str = n_to_str(a,n_bits_per_word,n_words,base) + ' '
+    b_str = n_to_str(b,n_bits_per_word,n_words,base) + ' '
+    n_str = n_to_str(n,n_bits_per_word,n_words,base) + ' '
+    nn0_str = nn[-n_symbols_per_word:] + '\r\n'
+    str_out = a_str + b_str + n_str + nn0_str
+    return str_out
 
 
 
---
+++ Sources/Python/tb_vec_gen/tv_gen_script.py
@@ -0,0 +1,45 @@
+import tb_vec_gen as tbm
+import math as m
+import gmpy2 as g
+
+a = 0xe41be5bde54ea01c5fd8132dae3c50bd9f96c5af1324a68d08d978048f69bf76
+b= 0xf9852cd21de6a57f70ba175bea2fffc2a40a26a7d424cf6f3cc8843f9135d1ed
+n= 0xb6fb5f6da48f54fc63af7b4b3e9c3631cd781a526fb464a66bb3e127e74e2c25
+r= 0x10000000000000000000000000000000000000000000000000000000000000000
+
+test_vec_256_1 = {
+    a: 0xe41be5bde54ea01c5fd8132dae3c50bd9f96c5af1324a68d08d978048f69bf76,
+    b: 0xf9852cd21de6a57f70ba175bea2fffc2a40a26a7d424cf6f3cc8843f9135d1ed,
+    n: 0xb6fb5f6da48f54fc63af7b4b3e9c3631cd781a526fb464a66bb3e127e74e2c25,
+    r: 0x10000000000000000000000000000000000000000000000000000000000000000
+}
+
+test_vec_256_2 = {
+    a: 0xa9bf5dff81ef3871f8206ecdd8b673cfb0b6737d5a983628babdca532e76f1ef,
+    b: 0xc893cd7b64513b7a2652d8e4865dc4d07af5bb3bd1ef10aa28197d81e622029f,
+    n: 0xbaa9614c0aff9d805f10ab09561f9bb9879bcf08bd25aa6955db00b696ac2173,
+    r: 0x10000000000000000000000000000000000000000000000000000000000000000
+}
+
+try_vec = {
+    a: 6,
+    b: 3,
+    n: 9,
+    r: 14
+}
+
+tv_list = [] #list containing test vectors to send to file
+
+tv_list.extend([test_vec_256_1,test_vec_256_2])
+
+
+file_lines =[]
+
+
+for i in tv_list:
+    file_lines.append(tbm.send_tv_str(*i.values(),64,4,16))
+
+file_lines[-1] = file_lines[-1][:-2]
+
+with open("out_results.txt","w") as ff:
+    ff.writelines(file_lines)
