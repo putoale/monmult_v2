cb2e6dc65584810edf50d245d34b6bd7351dbe8b
Alessandro Putortì <putoale@gmail.com>
2021-12-08 14:30:06 +0100

    added test vector read from file and script to compare results (WIP)

--- Sources/Python/tb_vec_gen/tb_vec_gen.py
+++ Sources/Python/tb_vec_gen/tb_vec_gen.py
@@ -24,30 +24,61 @@ def monmult_str (a,b,n,r,base=16):
     mult_str = g.digits(mmult,base)  # create output string
     return mult_str #return result of mult as string in the selected base
 
+def monmult_int (a,b,n,r):
+    """This function computes the modular multiplication and returns the result as an integer value"""
+
+    rr = g.invert(r,n) #compute inverse of r mod n
+    mmult = (int) (g.f_mod(g.mul(g.mul(a,b),rr),n)) #compute modular multiplication
+    return mmult #return result of mult as int
+
 
 def send_tv_str_oneline (a,b,n,r,n_bits_per_word,n_words,base=16):
     """This function takes a test vector (a,b,n,r), the number of words and of bits per word, and the base, and it returns a string (one line) 
-    in the format: <a b n n'(0)> """
+    in the format: <a  b  n  n'(0)>  separated by double spaces"""
 
     n_symbols_per_word = (int) (n_bits_per_word / m.log(base,2)) #compute number of symbols per word wrt base argument
 
     nn = g.digits(g.invert(-n,r),base).zfill(n_symbols_per_word*n_words) # compute n'
 
-    a_str = n_to_str(a,n_bits_per_word,n_words,base) + ' ' # create string containing "a" operator
-    b_str = n_to_str(b,n_bits_per_word,n_words,base) + ' ' # create string containing "b" operator
-    n_str = n_to_str(n,n_bits_per_word,n_words,base) + ' ' # create string containing "n"
+    a_str = n_to_str(a,n_bits_per_word,n_words,base) + '  ' # create string containing "a" operator
+    b_str = n_to_str(b,n_bits_per_word,n_words,base) + '  ' # create string containing "b" operator
+    n_str = n_to_str(n,n_bits_per_word,n_words,base) + '  ' # create string containing "n"
     nn0_str = nn[-n_symbols_per_word:] + '\r\n'            # create string containing n'(0)
     str_out = a_str + b_str + n_str + nn0_str              # compose output string
     return str_out                                         # return test vector data in a single line separated by spaces
 
-def load_tv_from_file(file_path,n_words,n_bits_per_word,base=16,show_nn0 = 1, show_r = 1):
+def load_tv_from_file(file_path,n_words,n_bits_per_word,r,base=16,show_nn0 = 1, show_r = 1):
     """This function loads test vectors from a file, and return a list of dictionaries with keys:
     a,b,n,nn0,r,golden_res,module_res (initialized to 0) """
 
     tv_list = []
-    dict_keys = ['a','b','n','nn0','r','golden_res','module_res']
+    file_lines =[]
+
+    dict_keys = ['a','b','n','nn0','r']
 
     n_symbols_per_word = (int) (n_bits_per_word / m.log(base,2))
 
+    with open(file_path,"r") as ff:
+        file_lines = ff.readlines()
     
+
+    for lin in file_lines.copy():
+
+        op_list = lin.split ("  ")
+        dict_values_str = [op.replace(" ","").replace("\n","").replace("\r","") for op in op_list] #stripped operators of 1 test vector
+        dict_values_int = [int(i,base=base) for i in dict_values_str]
+        dict_values_int.append(r)
+        tv_list.append(dict(zip(dict_keys,dict_values_int)))
+
+    return tv_list
+
+
+def load_res_from_file(file_path,base = 16):
+    """This function reads the results of the vhdl module from a file and returns them in a list"""
+
+    with open(file_path,"r") as ff:
+        file_lines = ff.readlines()
     
+    file_lines_stripped = [st.replace(" ","").replace("\n","").replace("\r","") for st in file_lines]
+    results_int = [int(st,base=base) for st in file_lines_stripped]
+    return results_int
--- Sources/Python/tb_vec_gen/tv_res_comp_script.py
+++ Sources/Python/tb_vec_gen/tv_res_comp_script.py
@@ -1,6 +1,38 @@
-# import os
-# script_dir = os.path.dirname(__file__)
-# rel_path = "txt"
-# input_file_name = "input_vectors.txt"
-# complete_path = os.path.join(script_dir,rel_path,input_file_name)
+import os
+import tb_vec_gen as tbm
+
+script_dir = os.path.dirname(__file__)
+vec_rel_path = "txt"
+vec_file_name = "input_vectors.txt"
+res_file_name = "out_results.txt"
+vec_f_cplt_path = os.path.join(script_dir,vec_rel_path,vec_file_name)
+tv_res_f_cplt_path = os.path.join(script_dir,vec_rel_path,res_file_name)
+
+
+n_words = 4
+n_bits_per_word = 64
+total_n_bits = n_words * n_bits_per_word
+r = pow(2,total_n_bits)
+
+
+#get list of sent test vectors from file
+tv_list = tbm.load_tv_from_file(vec_f_cplt_path,4,64,r)
+
+#add r value to tv_list
+[tv.update({'r': r}) for tv in tv_list]
+
+#compute golden results
+golden_list = [tbm.monmult_int(*list(tv.values())[:3],r) for tv in tv_list]
+
+
+#read module results from file
+module_results_list = tbm.load_res_from_file(tv_res_f_cplt_path)
+
+#add module results to tv_list
+for i in range(0,len(tv_list)):
+    tv_list[i].update({'module_results':module_results_list[i]})
+
+#add golden results to tv_list
+for i in range(0,len(tv_list)):
+    tv_list[i].update({'golden_result':golden_list[i]})
 
