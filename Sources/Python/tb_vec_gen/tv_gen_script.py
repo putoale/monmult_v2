23a98e1a58275e9d7b07847cd68fd90d4fc1846f
Alessandro Putortì <putoale@gmail.com>
2021-12-11 01:21:54 +0100

    Edited python scripts to make them accept parameters and produce general csv out (with all tv)

--- Sources/Python/tb_vec_gen/tv_gen_script.py
+++ Sources/Python/tb_vec_gen/tv_gen_script.py
@@ -1,67 +1,83 @@
+import sys
 import tb_vec_gen as tbm
 import math as m
 import gmpy2 as g
 import pprint as pp
+import os
+
+script_dir = os.path.dirname(__file__)
+rel_path = "txt"
+
+output_base = 16
 
 test_vec_256_1 = {
     'a': 0xe41be5bde54ea01c5fd8132dae3c50bd9f96c5af1324a68d08d978048f69bf76,
     'b': 0xf9852cd21de6a57f70ba175bea2fffc2a40a26a7d424cf6f3cc8843f9135d1ed,
     'n': 0xb6fb5f6da48f54fc63af7b4b3e9c3631cd781a526fb464a66bb3e127e74e2c25,
-    'r': 0x10000000000000000000000000000000000000000000000000000000000000000
+    'r': 0x10000000000000000000000000000000000000000000000000000000000000000,
+    'n_bits':256
 }
 
 test_vec_256_2 = {
     'a': 0xa9bf5dff81ef3871f8206ecdd8b673cfb0b6737d5a983628babdca532e76f1ef,
     'b': 0xc893cd7b64513b7a2652d8e4865dc4d07af5bb3bd1ef10aa28197d81e622029f,
     'n': 0xbaa9614c0aff9d805f10ab09561f9bb9879bcf08bd25aa6955db00b696ac2173,
-    'r': 0x10000000000000000000000000000000000000000000000000000000000000000
+    'r': 0x10000000000000000000000000000000000000000000000000000000000000000,
+    'n_bits':256
 }
 
 test_vec_32_2 = {
     'a': 0xabcdef99,
     'b': 0x9876abed,
     'n': 0xfde9fde9,
-    'r': 0x100000000
+    'r': 0x100000000,
+    'n_bits':32
 }
 
 test_vec_32_1 = {
     'a': 0x12345678,
     'b': 0x9abcdef1,
     'n': 0xfde9fde9,
-    'r': 0x100000000
+    'r': 0x100000000,
+    'n_bits':32
 }
 
-tv_256_list = [test_vec_256_1,test_vec_256_2]
+all_tv = [test_vec_256_1,test_vec_256_2,test_vec_32_1,test_vec_32_2] #list with all test vectors
 
-tv_32_list = [test_vec_32_1,test_vec_32_2]
+# you can call this script specifying a configuration. E.g. "python3 script 256_4_64 256_8_32 32_4_8" will print all 256 bits tv with 4 words and
+# 64 bits per word, in a file named "input_vectors_256_4_64" and so on...
+# If the script is called without any args, it assumes the default configuration
 
-tv_list = [] #list containing test vectors to send to file
-
-tv_list.extend([test_vec_256_1,test_vec_256_2])
-#tv_list.extend([test_vec_32_2,test_vec_32_2])
-#tv_list.append(test_vec_32_2)
-
-file_lines =[] #create empty list to send to file
-
-#populate file_lines from tv_list (256_4_64)
-for i in tv_list:
-    file_lines.append(tbm.send_tv_str_oneline(*i.values(),64,4,16))
-
-#populate file_lines from tv_list (16_4_8)
-#for i in tv_list:
-    #file_lines.append(tbm.send_tv_str_oneline(*i.values(),8,4,16))
-
-if len(file_lines) > 1:
-    file_lines[-1] = file_lines[-1][:-2] #remove last \n\r from output
+if len(sys.argv) > 1:
+    tv_config_list = sys.argv[1:]
 else:
-    file_lines[0] = file_lines[0][:-2] #remove last \n\r from output
+    tv_config_list = ["256_4_64","256_8_32","32_4_8"] #default configurations
 
-# find path of file to write to
-import os
-script_dir = os.path.dirname(__file__)
-rel_path = "txt"
-file_name = "input_vectors_256_4_64.txt"
-complete_path = os.path.join(script_dir,rel_path,file_name)
 
-with open(complete_path,"w") as ff:
-    ff.writelines(file_lines)
+tv_list = [] #list containing test vectors to send to file (At the moment it's populated but not used)
+
+for tv_conf in tv_config_list:
+
+    #parse the string to understand which tv_conf are needed
+    parsed_conf = [int(i) for i in tv_conf.split("_")]
+    curr_conf_list = [] #tv_list for current config only
+
+    #add to the final tv list all the test vectors with the desired n. of bits and configurations
+    for i in all_tv:
+
+        if i['n_bits'] == parsed_conf[0]:
+            i_copy = i.copy()
+            i_copy.update({'N_BITS_PER_WORD':parsed_conf[2],'N_WORDS':parsed_conf[1]})
+            tv_list.append(i_copy)
+            i_copy.pop('n_bits')
+            curr_conf_list.append(i_copy)
+    
+    #once tv_list is populated write test_vectors on files (one per config):
+    file_name  = "input_vectors_" + tv_conf + ".txt"
+
+    file_lines = [tbm.send_tv_str_oneline(*i.values(),base=output_base) for i in curr_conf_list]
+    file_lines[-1] = file_lines[-1][:-2] #remove last \n\r from output
+
+    file_path = os.path.join(script_dir,rel_path,file_name)
+    with open(file_path,"w") as ff:
+        ff.writelines(file_lines)
